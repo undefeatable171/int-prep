9341737651663eeea722ae2fae6ecf4d4a59e7f4
prathap-chowdary <166109930+prathap-chowdary@users.noreply.github.com>
2026-06-09 17:15:29 +0530

    Update content.js

--- azure/content.js
+++ azure/content.js
@@ -27,57 +27,6 @@ const qs = [
     <ul>
       <li>Data is encrypted both at rest and in transit.</li>
       <li>Supports Azure AD, Shared Access Signatures (SAS), and RBAC for access control.</li>
-    </ul>
-  </li>
-
-  <li>
-    <strong>Scalability</strong>
-    <ul>
-      <li>Storage capacity can grow or shrink based on business needs without hardware limitations.</li>
-    </ul>
-  </li>
-
-  <li>
-    <strong>Managed Service</strong>
-    <ul>
-      <li>Microsoft handles infrastructure maintenance, patching, and hardware management.</li>
-    </ul>
-  </li>
-
-  <li>
-    <strong>High Availability</strong>
-    <ul>
-      <li>Data replication ensures high availability even during hardware failures.</li>
-    </ul>
-  </li>const cats = ["storage","ADF","key-vault","CI-CD","Scenario"];
-const qs = [
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// new 
-
-{
-    cat:"storage",
-    q:"What is azure storage ",
-    answer:` 👉 Azure Storage is a cloud-based storage service provided by Microsoft Azure for storing and managing various types of data, such as files, images, videos, documents, backups, and application data.<br>
-
-👉 It supports both structured and unstructured data and allows access through multiple methods, including REST APIs, SDKs, HTTPS, Azure Portal, CLI, and PowerShell. It is designed to be highly available, durable, secure, and scalable.`,
-    children:[
-{
-    q:`What are the benefits of Azure Storage?`,
-    a:`
-<p>The key benefits of Azure Storage are:</p>
-
-<ol>
-  <li>
-    <strong>Durability</strong>
-    <ul>
-      <li>Azure automatically replicates data across multiple storage devices and, depending on the replication option, across regions to minimize data loss.</li>
-    </ul>
-  </li>
-
-  <li>
-    <strong>Security</strong>
-    <ul>
-      <li>Data is encrypted both at rest and in transit.</li>
-      <li>Supports Azure AD, Shared Access Signatures (SAS), and RBAC for access control.</li>
       <li>At rest : AES 256 encryptionand managed keys , HTTPS during transit. </li>
     </ul>
   </li>
@@ -245,18 +194,4 @@ children:[],
 ● This approach ensures secure, granular, and controlled access to sensitive data while minimizing security risks."`,
 children:[],
 },
-]
-
-  <li>
-    <strong>Accessibility</strong>
-    <ul>
-      <li>Data can be accessed through REST APIs, SDKs, Azure Portal, CLI, PowerShell, and HTTPS.</li>
-    </ul>
-  </li>
-</ol>`,
-    children:[],
-}
-              ],
-},
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ]
